f4321f46bdf3f12d778c123af3a07dc38984198c
Rodion <george.t.o.p@mail.ru>
2020-02-10 18:42:53 +0300

    добавлены getText().toString()

--- app/src/main/java/com/example/textstring/MainActivity.java
+++ app/src/main/java/com/example/textstring/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView resultField;
     private Button btnPoint;
+    Button btn0;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,39 +24,50 @@ public class MainActivity extends AppCompatActivity {
     public void numPress(View view) {
         int id = view.getId();
         if (id == R.id.btn0) {
+            resultField.getText().toString();
             resultField.setText("0");
         }
         if (id == R.id.btn1) {
+            resultField.getText().toString();
             resultField.setText("1");
         }
         if (id == R.id.btn2) {
+            resultField.getText().toString();
             resultField.setText("2");
         }
         if (id==R.id.btn3){
+            resultField.getText().toString();
             resultField.setText("3");
         }
         if (id==R.id.btn4){
+            resultField.getText().toString();
             resultField.setText("4");
         }
         if (id==R.id.btn5){
+            resultField.getText().toString();
             resultField.setText("5");
         }
         if (id==R.id.btn6){
+            resultField.getText().toString();
             resultField.setText("6");
         }
         if (id==R.id.btn7){
+            resultField.getText().toString();
             resultField.setText("7");
         }
         if (id==R.id.btn8){
+            resultField.getText().toString();
             resultField.setText("8");
         }
         if (id==R.id.btn9){
+            resultField.getText().toString();
             resultField.setText("9");
         }
     }
     public void dotPress(View v){
         int id = v.getId();
         if (id==R.id.btnPoint){
+            resultField.getText().toString();
             resultField.setText(".");
         }
     }
@@ -62,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         resultField  = findViewById(R.id.resultField);
         btnPoint = findViewById(R.id.btnPoint);
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
 
     }
 
